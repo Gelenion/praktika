77a018728c1ed8cf1c9508214da53cbee20d26f4
Gelenion <gelenion@gmail.com>
2025-01-12 01:29:09 +0200

    Update index.py

---
+++ index.py
@@ -0,0 +1,23 @@
+def print_3():
+    print(3)
+    
+    
+def print_2():
+    print(2)
+    
+    
+def print_1():
+    print(1)
+    
+    
+def print_hello():
+    print("hello")
+    
+    
+def print_hello_world():
+    print("hello world!")
+    
+    
+if "__main__" == __name__:
+     print_hello_world()  
+    
